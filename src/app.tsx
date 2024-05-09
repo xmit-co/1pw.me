d3e0cdd2e088fc8a6e00225d7157f6e7790484e0
Pierre Carrier <pc@rrier.fr>
2024-05-09 18:58:11 +0200

    wip

---
+++ src/app.tsx
@@ -0,0 +1,157 @@
+import { useEffect, useMemo, useState } from "preact/hooks";
+import nacl from "tweetnacl";
+import { Encoder } from "cbor-x";
+
+const EXPLANATION = `This is a simple editor that encrypts your content with a passphrase.\nIf you lose the passphrase, the content is lost forever.`;
+
+const subtle = window.crypto.subtle;
+
+const CBOROptions = {
+  useRecords: false,
+  mapsAsObjects: false,
+};
+
+const encoder = new Encoder(CBOROptions);
+
+export function App() {
+  const [error, setError] = useState<string | null>(null);
+  const [passphrase, setPassphrase] = useState<string | null>(null);
+  const [content, setContent] = useState<string | null>(null);
+  const [working, setWorking] = useState<boolean>(false);
+  const [seed, setSeed] = useState<Uint8Array | null>(null);
+
+  const message = useMemo(
+    () => new TextEncoder().encode(content || ""),
+    [content],
+  );
+  const length = message.length;
+  const lengthError = length > 64000;
+
+  useEffect(() => {
+    setWorking(true);
+    (async () => {
+      const key = await subtle.importKey(
+        "raw",
+        new TextEncoder().encode(passphrase || ""),
+        "PBKDF2",
+        false,
+        ["deriveBits"],
+      );
+      const bits = await subtle.deriveBits(
+        {
+          name: "PBKDF2",
+          hash: "SHA-256",
+          salt: new TextEncoder().encode("1pw.me"),
+          iterations: 100000,
+        },
+        key,
+        256,
+      );
+      setSeed(new Uint8Array(bits));
+      setWorking(false);
+    })().catch((e) => {
+      setError(e.message);
+    });
+  }, [passphrase]);
+
+  return (
+    <div class="editor">
+      <h1>🔏 1pw.me — passphrase → page</h1>
+      <div>
+        <input
+          type="password"
+          placeholder="Passphrase"
+          value={passphrase || ""}
+          onInput={(e) => setPassphrase((e.target as HTMLInputElement).value)}
+        />
+        <button
+          disabled={working}
+          onClick={() => {
+            setWorking(true);
+            (async () => {
+              if (seed === null) {
+                setError("No passphrase");
+                return;
+              }
+              const signKP = nacl.sign.keyPair.fromSeed(seed);
+              const boxKP = nacl.box.keyPair.fromSecretKey(seed);
+              const postBytes = encoder.encode([signKP.publicKey]);
+              const result = await fetch("https://0pw.me", {
+                method: "POST",
+                body: postBytes,
+              });
+              if (!result.ok) {
+                setError(`${result.status} ${result.statusText}`);
+                return;
+              }
+              const signed = await result.arrayBuffer();
+              const payload = nacl.sign.open(
+                new Uint8Array(signed),
+                signKP.publicKey,
+              );
+              if (payload === null) {
+                setError("Signature verification failed");
+                return;
+              }
+              const [nonce, box] = encoder.decode(new Uint8Array(payload));
+              const message = nacl.secretbox.open(box, nonce, boxKP.secretKey);
+              if (message === null) {
+                setError("Decryption failed");
+                return;
+              }
+              setContent(new TextDecoder().decode(message));
+              setError(null);
+            })()
+              .catch((e) => setError(e.message))
+              .finally(() => {
+                setWorking(false);
+              });
+          }}
+        >
+          Load
+        </button>
+        <button
+          disabled={working || lengthError}
+          onClick={() => {
+            setWorking(true);
+            (async () => {
+              if (seed === null) {
+                setError("No passphrase");
+                return;
+              }
+              const signKP = nacl.sign.keyPair.fromSeed(seed);
+              const boxKP = nacl.box.keyPair.fromSecretKey(seed);
+              const nonce = nacl.randomBytes(nacl.box.nonceLength);
+              const box = nacl.secretbox(message, nonce, boxKP.secretKey);
+              const payload = encoder.encode([nonce, box]);
+              const signed = nacl.sign(payload, signKP.secretKey);
+              const postBytes = encoder.encode([signKP.publicKey, signed]);
+              const result = await fetch("https://0pw.me", {
+                method: "POST",
+                body: postBytes,
+              });
+              if (!result.ok) {
+                setError(`${result.status} ${result.statusText}`);
+              } else {
+                setError(null);
+              }
+            })()
+              .catch((e) => setError(e.message))
+              .finally(() => {
+                setWorking(false);
+              });
+          }}
+        >
+          Save
+        </button>
+        {error && <span class="error">{error}</span>}
+      </div>
+      <textarea
+        value={content || ""}
+        placeholder={EXPLANATION}
+        onInput={(e) => setContent((e.target as HTMLTextAreaElement).value)}
+      ></textarea>
+      <div class={(lengthError && "error") || undefined}>{length} / 64000</div>
+    </div>
+  );
+}
